bcd4aacf43256ade755ce90fc92a6aea5a1f68cf
Jasmine <105261487+codewithjazzy@users.noreply.github.com>
2025-07-03 17:08:24 -0700

    configured clerk and created signin/signup pages

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 export default function HomePage() {
   return (
-    <div className='text-red-500'>
-      HomePage
+    <div>
+      <h1 className='text-3xl font-bold mb-4'>Sleep Tracker</h1>
+      <div className='text-red-500'>HomePage</div>
     </div>
   )
 }
